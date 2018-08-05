64540cd6e5ace1a247e5318f53becd2571d79878
whydonti <nikhilbhandarkar1998@gmail.com>
2018-08-05 09:55:01 +0530

    qwerty

--- sorting/quickSort.cpp
+++ sorting/quickSort.cpp
@@ -18,35 +18,40 @@ void swap(int *a, int* b){
 	*b = temp;
 }
 
-int getMinimum(vector <int> v, int s, int e){
-	int min_index = v[0];
+int partition(vector <int> *v, int s, int e){
+	int j = s;
+	int pivot = v->at(e);
 	for(int i=s; i<e; i++){
-		if(v[i] < v[min_index]){
-			min_index = i;
+		if(j<e && v->at(i) < pivot){
+			swap(v->at(i), v->at(j));
+			j++;
 		}
 	}
-	return min_index;
+	swap(v->at(e), v->at(j));
+	return j;
 }
 
-int quickSort(vector <int> v){
-	int len = v.size();
-	int pivot = v[len-1];
-	for(int i=){
-
+int quickSort(vector <int> *v, int s, int e, int count){
+	if(s <= e){
+		int pivot = partition(v, s, e);
+		quickSort(v, s, pivot-1, ++count);
+		quickSort(v, pivot+1, e, ++count);
 	}
-	printArray(v);
 }
 
 int main()
 {
 	/* code */
 	vector <int> v;
-	int n, ip;cin>>n;
+	int n, ip, len, count=0;cin>>n;
 	while(n--){
 		cin>>ip;
 		v.push_back(ip);
 	}
-
-	quickSort(v);
+	len = v.size();
+	count = 0;
+	quickSort(&v, 0, len-1, ++count);
+	printArray(v);
+	cout<<endl;
 	return 0;
 }
